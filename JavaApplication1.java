5b95dedc21f201bc9745219c62b29518ba3c567c
MustafaKaraca17 <79407902+MustafaKaraca17@users.noreply.github.com>
2021-12-12 14:24:29 +0100

    eklendi

---
+++ JavaApplication1.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author musta
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int x = 10;
+        int y;
+        int Mustafa;
+        Mustafa = 10;
+        System.out.println("hello World" + Mustafa);
+        Mustafa = 20;
+        System.out.println("hello World" + Mustafa);
+        float pi = (float) 3.14;
+        String s = "Benim adim Mustafa Karaca";
+        System.out.println(" dizgi "+ s);
+        System.out.println(" pi : "+ pi);
+        //System.out.println("Mustafa Karaca");
+        //unutmamak icin notaliyorum
+        /* unutma 1 
+        unutma2
+        unutma3
+        */
+    }
+    
+}
